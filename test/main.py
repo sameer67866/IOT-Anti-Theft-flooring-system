9b54018b3dcabe5e2dbe198f65af38410f8c1e2e
VetinariLives <talha32189@gmail.com>
2023-11-27 14:16:25 -0500

    working servo and sensors

--- test/main.py
+++ test/main.py
@@ -1,11 +1,11 @@
 import sensor
-import servo_motor  
+import servo_motor
 import time
 # import camera
 # import coap_communication
 def main():
     piezo_sensor = sensor.PiezoSensor()
-    current_servo_angle = 60  # Starting angle
+    current_servo_angle = 45  # Starting angle
     servo=servo_motor.ServoMotor()
     servo.set_angle( current_servo_angle)  # Initialize servo to start position
 
@@ -15,7 +15,7 @@ def main():
     end_time = time.time() + warmup_time
     while time.time() < end_time:
         piezo_sensor.read_sensors()  # Read sensors to let them stabilize
-        time.sleep(0.5)  # Sleep a bit between readings
+        time.sleep(0.1)  # Sleep a bit between readings
 
     print("Sensors stabilized, starting motion detection.")
 
@@ -23,14 +23,18 @@ def main():
 
     while True:
         readings_1, readings_2 = piezo_sensor.read_sensors()
-        
+
+        print(f"tile 1 avg: {readings_1[0]}, Tile2:{readings_2[0]}")
+
         # Check if there's a significant change in pressure on either tile
         if abs(readings_1[0] - last_reading_1[0]) > 5000:  # Tile 1
-            current_servo_angle = max(60, current_servo_angle - 30)  # Move 30 degrees to the left
-            servo.set_angle(current_servo_angle)
+            if (current_servo_angle !=70):
+                 current_servo_angle = 70  # Move 30 degrees to the left
+                 servo.set_angle(current_servo_angle)
         elif abs(readings_2[0] - last_reading_2[0]) > 5000:  # Tile 2
-            current_servo_angle = min(120, current_servo_angle + 30)  # Move 30 degrees to the right
-            servo.set_angle(current_servo_angle)
+            if (current_servo_angle != 20):
+            	current_servo_angle = 20  # Move 30 degrees to the right
+            	servo.set_angle(current_servo_angle)
 
         # Update last readings
         last_reading_1, last_reading_2 = readings_1, readings_2
