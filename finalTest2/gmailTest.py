b6c9a0a8c9ec30132c6340c64a9c7efe03d972a0
sameer67866 <113478060+sameer67866@users.noreply.github.com>
2023-11-28 16:23:24 -0500

    gmailTest.py with capibility to send images

--- finalTest2/gmailTest.py
+++ finalTest2/gmailTest.py
@@ -1,32 +1,34 @@
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-import smtplib
 import os
 
-recipient = "musabtree@hotmail.com"
-sender = "cyber.talha0@gmail.com"
-subject = "Test Email with Image"
-text = "Hello world, this is an email with an image!"
+# Hardcode the directory path
+directory_path = "/Downloads/IOT-Anti-Theft-flooring-system/finalTest2"
 
-# Create a multipart message
-message = MIMEMultipart()
-message["From"] = sender
-message["To"] = recipient
-message["Subject"] = subject
+def send_email_with_filename(filename):
+    recipient = "musabtree@hotmail.com"
+    sender = "cyber.talha0@gmail.com"
+    subject = "Test Email with Image"
+    text = "Hello world, this is an email with an image!"
 
-# Attach the text part
-message.attach(MIMEText(text, "plain"))
+    # Create a multipart message
+    message = MIMEMultipart()
+    message["From"] = sender
+    message["To"] = recipient
+    message["Subject"] = subject
 
-# Attach the image
-image_path = 'path/to/your/image.jpg'
-with open(image_path, 'rb') as img_file:
-    img = MIMEImage(img_file.read(), name=os.path.basename(image_path))
-message.attach(img)
+    # Attach the text part
+    message.attach(MIMEText(text, "plain"))
 
-# Send the email
-smtp = smtplib.SMTP("smtp.gmail.com", port=587)
-smtp.starttls()
-smtp.login(sender, "qlyk rwjf lvhj nnql")  # Use your App Password here
-smtp.send_message(message)
-smtp.quit()
+    # Create the full path to the image using the provided filename and hardcoded path
+    full_path = os.path.join(directory_path, filename)
+
+    # Attach the image
+    with open(full_path, 'rb') as img_file:
+        img = MIMEImage(img_file.read(), name=os.path.basename(full_path))
+    message.attach(img)
+
+    # Send the email
+    smtp = smtplib.SMTP("smtp.gmail.com", port=587)
+    smtp.starttls()
+    smtp.login(sender, "qlyk rwjf lvhj nnql")  # Use your App Password here
+    smtp.send_message(message)
+    smtp.quit()
