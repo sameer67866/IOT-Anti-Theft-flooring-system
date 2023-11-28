f98280d819e4404bbbbb86a5d88bd7026966b5ca
VetinariLives <talha32189@gmail.com>
2023-11-28 14:33:30 -0500

    debugged code

--- finalTest2/camTest.py
+++ finalTest2/camTest.py
@@ -9,17 +9,18 @@ def take_picture():
     picam.start_preview(Preview.QTGL)
     picam.start()
     time.sleep(2)
-    
+
     # Generate filename based on current date and time
-    filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".jpg"
-    picam.capture_file(filename)
+   # filename = "img1.jpg"
+#"%Y-%m-%d_%H-%M-%S") + ".jpg"
+    picam.capture_file("file.jpg")
     picam.close()
     return filename
 
 def test_take_picture():
     print("Taking a picture...")
     filename = take_picture()
-    print(f"Picture taken and saved as: {filename}")
+
 
 # Call the test function
 test_take_picture()
