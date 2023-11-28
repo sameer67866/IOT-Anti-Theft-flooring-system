f4d04f693945793a1d906ff0b3b0e325d9a154c3
sameer67866 <113478060+sameer67866@users.noreply.github.com>
2023-11-28 14:47:43 -0500

    Update camTest.py

--- finalTest2/camTest.py
+++ finalTest2/camTest.py
@@ -2,17 +2,21 @@ import time
 from picamera2 import Picamera2, Preview
 from datetime import datetime
 
+# Initialize and start the camera when the module is imported
+picam = Picamera2()
+config = picam.create_preview_configuration()
+picam.configure(config)
+picam.start_preview(Preview.QTGL)
+picam.start()
+time.sleep(2)  # Camera warm-up time
+
 def take_picture():
-    picam = Picamera2()
-    config = picam.create_preview_configuration()
-    picam.configure(config)
-    picam.start_preview(Preview.QTGL)
-    picam.start()
-    time.sleep(2)
-    
     # Generate filename based on current date and time
     filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".jpg"
     picam.capture_file(filename)
-    picam.close()
     return filename
+
+def stop_camera():
+    picam.stop_preview()
+    picam.close()
 
