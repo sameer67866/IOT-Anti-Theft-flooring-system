f02ffe8a46a28daac56769e80d06117eff2390b7
sameer67866 <113478060+sameer67866@users.noreply.github.com>
2023-11-28 13:46:05 -0500

    Update gmailTest.py

--- finalTest2/gmailTest.py
+++ finalTest2/gmailTest.py
@@ -1,18 +1,19 @@
 from email.message import EmailMessage
 import smtplib
 
-recipient = "musabtree@hotmail.com"
-sender = "cyber.talha0@gmail.com"
-message = "Hello world!"
+def send_email_with_filename(filename):
+    recipient = "musabtree@hotmail.com"
+    sender = "cyber.talha0@gmail.com"
+    message = f"Image captured: {filename}"
 
-email = EmailMessage()
-email["From"] = sender
-email["To"] = recipient
-email["Subject"] = "Test Email"
-email.set_content(message)
+    email = EmailMessage()
+    email["From"] = sender
+    email["To"] = recipient
+    email["Subject"] = "Alert: Motion Detected"
+    email.set_content(message)
 
-smtp = smtplib.SMTP("smtp.gmail.com", port=587)
-smtp.starttls()
-smtp.login(sender, "qlyk rwjf lvhj nnql")
-smtp.sendmail(sender, recipient, email.as_string())
-smtp.quit()
+    smtp = smtplib.SMTP("smtp.gmail.com", port=587)
+    smtp.starttls()
+    smtp.login(sender, "qlyk rwjf lvhj nnql") 
+    smtp.sendmail(sender, recipient, email.as_string())
+    smtp.quit()
