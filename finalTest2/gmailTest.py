087255bf5b3df390b5cf0f5e2a393c040a0ee079
sameer67866 <113478060+sameer67866@users.noreply.github.com>
2023-11-28 16:01:56 -0500

    Update gmailTest.py

--- finalTest2/gmailTest.py
+++ finalTest2/gmailTest.py
@@ -1,19 +1,32 @@
-from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 import smtplib
+import os
 
-def send_email_with_filename(filename):
-    recipient = "musabtree@hotmail.com"
-    sender = "cyber.talha0@gmail.com"
-    message = f"Image captured: {filename}"
+recipient = "musabtree@hotmail.com"
+sender = "cyber.talha0@gmail.com"
+subject = "Test Email with Image"
+text = "Hello world, this is an email with an image!"
 
-    email = EmailMessage()
-    email["From"] = sender
-    email["To"] = recipient
-    email["Subject"] = "Alert: Motion Detected"
-    email.set_content(message)
+# Create a multipart message
+message = MIMEMultipart()
+message["From"] = sender
+message["To"] = recipient
+message["Subject"] = subject
 
-    smtp = smtplib.SMTP("smtp.gmail.com", port=587)
-    smtp.starttls()
-    smtp.login(sender, "qlyk rwjf lvhj nnql") 
-    smtp.sendmail(sender, recipient, email.as_string())
-    smtp.quit()
+# Attach the text part
+message.attach(MIMEText(text, "plain"))
+
+# Attach the image
+image_path = 'path/to/your/image.jpg'
+with open(image_path, 'rb') as img_file:
+    img = MIMEImage(img_file.read(), name=os.path.basename(image_path))
+message.attach(img)
+
+# Send the email
+smtp = smtplib.SMTP("smtp.gmail.com", port=587)
+smtp.starttls()
+smtp.login(sender, "qlyk rwjf lvhj nnql")  # Use your App Password here
+smtp.send_message(message)
+smtp.quit()
