9567404cbc3a15a5bc2170cad4e872bd91909900
sameer67866 <113478060+sameer67866@users.noreply.github.com>
2023-11-28 15:59:49 -0500

    Create emailTest.py

---
+++ finalTest2/emailTest.py
@@ -0,0 +1,20 @@
+# woking email test with simple file name of image
+from email.message import EmailMessage
+import smtplib
+
+def send_email_with_filename(filename):
+    recipient = "musabtree@hotmail.com"
+    sender = "cyber.talha0@gmail.com"
+    message = f"Image captured: {filename}"
+
+    email = EmailMessage()
+    email["From"] = sender
+    email["To"] = recipient
+    email["Subject"] = "Alert: Motion Detected"
+    email.set_content(message)
+
+    smtp = smtplib.SMTP("smtp.gmail.com", port=587)
+    smtp.starttls()
+    smtp.login(sender, "qlyk rwjf lvhj nnql") 
+    smtp.sendmail(sender, recipient, email.as_string())
+    smtp.quit()
