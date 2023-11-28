8f4f74ccd35fee733f9e1607accfbc08e13fe7e9
sameer67866 <113478060+sameer67866@users.noreply.github.com>
2023-11-28 15:56:29 -0500

    Create camTest2.py

---
+++ finalTest2/camTest2.py
@@ -0,0 +1,17 @@
+import time
+from picamera2 import Picamera2, Preview
+from datetime import datetime
+
+def take_picture():
+    picam = Picamera2()
+    config = picam.create_preview_configuration()
+    picam.configure(config)
+    picam.start_preview(Preview.QTGL)
+    picam.start()
+    time.sleep(2)
+    
+    # Generate filename based on current date and time
+    filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".jpg"
+    picam.capture_file(filename)
+    picam.close()
+    return filename
